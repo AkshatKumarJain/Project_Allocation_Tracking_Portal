8cac1cff2ffc783407cefb6fd3e9c1a6d3ded558
Aditya Virmani <adityavirmani4125@gmail.com>
2026-09-09 20:48:37 +0530

    fix: use POST method for user login route

--- Backend/src/modules/users/user.route.ts
+++ Backend/src/modules/users/user.route.ts
@@ -7,7 +7,7 @@ const requireAuth = authMiddleware({ required: true }) as RequestHandler;
 
 const router = express.Router();
 
-router.get("/login", userController.login);
+router.post("/login", userController.login);
 router.post("/logout", requireAuth, userController.logout);
 router.post("/refresh", requireAuth, userController.refresh);
 
