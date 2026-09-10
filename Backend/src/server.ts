a0c8179d0c05602cf675bc32fe1091ac4b8fdfd0
Aditya Virmani <adityavirmani4125@gmail.com>
2026-09-10 21:15:35 +0530

    fix: connect frontend with backend authentication

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import "dotenv/config"
+import cors from "cors";
 import { connectDB } from "./config/db";
 import { errorHandler } from "./middlewares/errorHandler";
 import userRouter from "./modules/users/user.route";
 import {connectRedis, disconnectRedis} from "./config/redis";
 
 const app = express();
-
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 app.use(express.json());
 
 connectRedis();
